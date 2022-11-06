c872d35f90e5406e611b584739fa651206ec4cdb
Ithink322 <107264542+Ithink322@users.noreply.github.com>
2022-11-06 09:00:50 +0300

    Ice_cream.color
    my_first_heavy_project

---
+++ work/script.js
@@ -0,0 +1,293 @@
+// Mini_Menu and SingleMenu start //
+/* open .singlemenu_vanilla_ball(#FFDBC7, SingleMenu) due to click on (img) .vanilla_ball */
+$(function () {
+  $(".vanilla_ball").on("click", function () {
+    $(".singlemenu_vanilla_ball").fadeIn(210);
+    $(
+      ".blue_rect_for_i-c_vanila_corn, .blue_rect_for_i-c_vanila_cornchik, .blue_rect_for_i-c_wh-chokolate_stick, .plum_rect_for_i-c_mix, .plum_rect_for_banki"
+    ).css("display", "none"); //hiding MiniMenus
+  });
+});
+/* close .singlemenu_vanilla_ball(#FFDBC7, SingleMenu) due to click on (img) .singlemenu_cross */
+$(function () {
+  $(".singlemenu_cross").on("click", function () {
+    $(".singlemenu_vanilla_ball").fadeOut(100);
+  });
+});
+
+/* open .singlemenu_chokolate_ball(#FFDBC7, SingleMenu) due to click on (img) .chokolate_ball */
+$(function () {
+  $(".chokolate_ball").on("click", function () {
+    $(".singlemenu_chokolate_ball").fadeIn(210);
+    $(
+      ".blue_rect_for_i-c_vanila_corn, .blue_rect_for_i-c_vanila_cornchik, .blue_rect_for_i-c_wh-chokolate_stick, .plum_rect_for_i-c_mix, .plum_rect_for_banki"
+    ).css("display", "none"); //hiding MiniMenus
+  });
+});
+/* close .singlemenu_chokolate_ball(#FFDBC7, SingleMenu) due to click on (img) .singlemenu_cross */
+$(function () {
+  $(".singlemenu_cross").on("click", function () {
+    $(".singlemenu_chokolate_ball").fadeOut(100);
+  });
+});
+
+/* open .singlemenu_pistachio_ball(#FFDBC7, SingleMenu) due to click on (img) .fistash_ball */
+$(function () {
+  $(".fistash_ball").on("click", function () {
+    $(".singlemenu_pistachio_ball").fadeIn(210);
+    $(
+      ".blue_rect_for_i-c_vanila_corn, .blue_rect_for_i-c_vanila_cornchik, .blue_rect_for_i-c_wh-chokolate_stick, .plum_rect_for_i-c_mix, .plum_rect_for_banki"
+    ).css("display", "none"); //hiding MiniMenus
+  });
+});
+/* close .singlemenu_pistachio_ball(#FFDBC7, SingleMenu) due to click on (img) .singlemenu_cross */
+$(function () {
+  $(".singlemenu_cross").on("click", function () {
+    $(".singlemenu_pistachio_ball").fadeOut(100);
+  });
+});
+
+/* open .blue_rect_for_i-c_vanila_corn(#93BFD8, MiniMenu) due to click on (img) .i-c_vanila_corn */
+$(function () {
+  $(".i-c_vanila_corn").on("click", function () {
+    $(".blue_rect_for_i-c_vanila_corn").fadeIn(600);
+  });
+});
+/* open .singlemenu_valila_corn(#FFDBC7, SingleMenu) due to click on (img) .vanila_corn_MINI */
+$(function () {
+  $(".vanila_corn_MINI").on("click", function () {
+    $(".singlemenu_valila_corn").fadeIn(390);
+    $(
+      ".blue_rect_for_i-c_vanila_corn, .blue_rect_for_i-c_vanila_cornchik, .blue_rect_for_i-c_wh-chokolate_stick, .plum_rect_for_i-c_mix, .plum_rect_for_banki"
+    ).css("display", "none"); //hiding MiniMenus
+  });
+});
+/* close .singlemenu_valila_corn(#FFDBC7, SingleMenu) due to click on (img) .singlemenu_cross */
+$(function () {
+  $(".singlemenu_cross").on("click", function () {
+    $(".singlemenu_valila_corn").fadeOut(100);
+  });
+});
+
+/* open .singlemenu_chokolate_corn(#FFDBC7, SingleMenu) due to click on (img) .chockolate_corn_MINI */
+$(function () {
+  $(".chockolate_corn_MINI").on("click", function () {
+    $(".singlemenu_chokolate_corn").fadeIn(210);
+    $(
+      ".blue_rect_for_i-c_vanila_corn, .blue_rect_for_i-c_vanila_cornchik, .blue_rect_for_i-c_wh-chokolate_stick, .plum_rect_for_i-c_mix, .plum_rect_for_banki"
+    ).css("display", "none"); //hiding MiniMenus
+  });
+});
+/* close .(#FFDBC7, SingleMenu) due to click on (img) .singlemenu_cross */
+$(function () {
+  $(".singlemenu_cross").on("click", function () {
+    $(".singlemenu_chokolate_corn").fadeOut(100);
+  });
+});
+
+/* open .singlemenu_strawberry_corn(#FFDBC7, SingleMenu) due to click on (img) .strawberry_corn_MINI */
+$(function () {
+  $(".strawberry_corn_MINI").on("click", function () {
+    $(".singlemenu_strawberry_corn").fadeIn(210);
+    $(
+      ".blue_rect_for_i-c_vanila_corn, .blue_rect_for_i-c_vanila_cornchik, .blue_rect_for_i-c_wh-chokolate_stick, .plum_rect_for_i-c_mix, .plum_rect_for_banki"
+    ).css("display", "none"); //hiding MiniMenus
+  });
+});
+/* close .singlemenu_strawberry_corn(#FFDBC7, SingleMenu) due to click on (img) .singlemenu_cross */
+$(function () {
+  $(".singlemenu_cross").on("click", function () {
+    $(".singlemenu_strawberry_corn").fadeOut(100);
+  });
+});
+
+/* open .singlemenu_ananas_corn(#FFDBC7, SingleMenu) due to click on (img) .ananas_corn_MINI */
+$(function () {
+  $(".ananas_corn_MINI").on("click", function () {
+    $(".singlemenu_ananas_corn").fadeIn(210);
+    $(
+      ".blue_rect_for_i-c_vanila_corn, .blue_rect_for_i-c_vanila_cornchik, .blue_rect_for_i-c_wh-chokolate_stick, .plum_rect_for_i-c_mix, .plum_rect_for_banki"
+    ).css("display", "none"); //hiding MiniMenus
+  });
+});
+/* close .singlemenu_ananas_corn(#FFDBC7, SingleMenu) due to click on (img) .singlemenu_cross */
+$(function () {
+  $(".singlemenu_cross").on("click", function () {
+    $(".singlemenu_ananas_corn").fadeOut(100);
+  });
+});
+
+/* open .blue_rect_for_i-c_vanila_cornchik(#93BFD8, MiniMenu) due to click on (img) .i-c_vanila_cornchik */
+$(function () {
+  $(".i-c_vanila_cornchik").on("click", function () {
+    $(".blue_rect_for_i-c_vanila_cornchik").fadeIn(600);
+  });
+});
+/* open .singlemenu_vanila_cornchik(#FFDBC7, SingleMenu) due to click on (img) .vanila_cornchick_MINI */
+$(function () {
+  $(".vanila_cornchick_MINI").on("click", function () {
+    $(".singlemenu_vanila_cornchik").fadeIn(210);
+    $(
+      ".blue_rect_for_i-c_vanila_corn, .blue_rect_for_i-c_vanila_cornchik, .blue_rect_for_i-c_wh-chokolate_stick, .plum_rect_for_i-c_mix, .plum_rect_for_banki"
+    ).css("display", "none"); //hiding MiniMenus
+  });
+});
+/* close .singlemenu_vanila_cornchik(#93BFD8, SingleMenu) due to click on (img) .singlemenu_cross */
+$(function () {
+  $(".singlemenu_cross").on("click", function () {
+    $(".singlemenu_vanila_cornchik").fadeOut(100);
+  });
+});
+
+/* open .singlemenu_chokolate_cornchik(#FFDBC7, SingleMenu) due to click on (img) .chokolate_cornchik_MINI */
+$(function () {
+  $(".chokolate_cornchik_MINI").on("click", function () {
+    $(".singlemenu_chokolate_cornchik").fadeIn(210);
+    $(
+      ".blue_rect_for_i-c_vanila_corn, .blue_rect_for_i-c_vanila_cornchik, .blue_rect_for_i-c_wh-chokolate_stick, .plum_rect_for_i-c_mix, .plum_rect_for_banki"
+    ).css("display", "none"); //hiding MiniMenus
+  });
+});
+/* close .singlemenu_chokolate_cornchik(#FFDBC7, SingleMenu) due to click on (img) .singlemenu_cross */
+$(function () {
+  $(".singlemenu_cross").on("click", function () {
+    $(".singlemenu_chokolate_cornchik").fadeOut(100);
+  });
+});
+
+/* open .blue_rect_for_i-c_wh-chokolate_stick(#93BFD8, MiniMenu) due to click on (img) .i-c_wh-shop_stick */
+$(function () {
+  $(".i-c_wh-shop_stick").on("click", function () {
+    $(".blue_rect_for_i-c_wh-chokolate_stick").fadeIn(600);
+  });
+});
+/* open .singlemenu_wh-chokolate_stick(#FFDBC7, SingleMenu) due to click on (img) .wh_stick_MINI */
+$(function () {
+  $(".wh_stick_MINI").on("click", function () {
+    $(".singlemenu_wh-chokolate_stick").fadeIn(210);
+    $(
+      ".blue_rect_for_i-c_vanila_corn, .blue_rect_for_i-c_vanila_cornchik, .blue_rect_for_i-c_wh-chokolate_stick, .plum_rect_for_i-c_mix, .plum_rect_for_banki"
+    ).css("display", "none"); //hiding MiniMenus
+  });
+});
+/* close .singlemenu_wh-chokolate_stick(#FFDBC7, SingleMenu) due to click on (img) .singlemenu_cross */
+$(function () {
+  $(".singlemenu_cross").on("click", function () {
+    $(".singlemenu_wh-chokolate_stick").fadeOut(100);
+  });
+});
+
+/* open .singlemenu_chokolate_stick(#FFDBC7, SingleMenu) due to click on (img) .chockolate_stick_MINI */
+$(function () {
+  $(".chockolate_stick_MINI").on("click", function () {
+    $(".singlemenu_chokolate_stick").fadeIn(210);
+    $(
+      ".blue_rect_for_i-c_vanila_corn, .blue_rect_for_i-c_vanila_cornchik, .blue_rect_for_i-c_wh-chokolate_stick, .plum_rect_for_i-c_mix, .plum_rect_for_banki"
+    ).css("display", "none"); //hiding MiniMenus
+  });
+});
+/* close .singlemenu_chokolate_stick(#FFDBC7, SingleMenu) due to click on (img) .singlemenu_cross */
+$(function () {
+  $(".singlemenu_cross").on("click", function () {
+    $(".singlemenu_chokolate_stick").fadeOut(100);
+  });
+});
+
+/* open .plum_rect_for_i-c_mix(#93BFD8, MiniMenu) due to click on (img) .i-c_mix_shop */
+$(function () {
+  $(".i-c_mix_shop").on("click", function () {
+    $(".plum_rect_for_i-c_mix").fadeIn(420);
+  });
+});
+/* open .singlemenu_mix_cream(#FFDBC7, SingleMenu) due to click on (img) .mix_MINI */
+$(function () {
+  $(".mix_MINI").on("click", function () {
+    $(".singlemenu_mix_cream").fadeIn(210);
+    $(
+      ".blue_rect_for_i-c_vanila_corn, .blue_rect_for_i-c_vanila_cornchik, .blue_rect_for_i-c_wh-chokolate_stick, .plum_rect_for_i-c_mix, .plum_rect_for_banki"
+    ).css("display", "none"); //hiding MiniMenus
+  });
+});
+/* close .singlemenu_mix_cream(#FFDBC7, SingleMenu) due to click on (img) .singlemenu_cross */
+$(function () {
+  $(".singlemenu_cross").on("click", function () {
+    $(".singlemenu_mix_cream").fadeOut(100);
+  });
+});
+
+/* open .singlemenu_jem(#FFDBC7, SingleMenu) due to click on (img) .jem_MINI */
+$(function () {
+  $(".jem_MINI").on("click", function () {
+    $(".singlemenu_jem").fadeIn(210);
+    $(
+      ".blue_rect_for_i-c_vanila_corn, .blue_rect_for_i-c_vanila_cornchik, .blue_rect_for_i-c_wh-chokolate_stick, .plum_rect_for_i-c_mix, .plum_rect_for_banki"
+    ).css("display", "none"); //hiding MiniMenus
+  });
+});
+/* close .singlemenu_jem(#FFDBC7, SingleMenu) due to click on (img) .singlemenu_cross */
+$(function () {
+  $(".singlemenu_cross").on("click", function () {
+    $(".singlemenu_jem").fadeOut(100);
+  });
+});
+
+/* open .plum_rect_for_banki(#93BFD8, MiniMenu) due to click on (img) .banki_transparent_shop */
+$(function () {
+  $(".banki_transparent_shop").on("click", function () {
+    $(".plum_rect_for_banki").fadeIn(420);
+  });
+});
+/* open .singlemenu_banki(#FFDBC7, SingleMenu) due to click on (img) .banki_MINI */
+$(function () {
+  $(".banki_MINI").on("click", function () {
+    $(".singlemenu_banki").fadeIn(210);
+    $(
+      ".blue_rect_for_i-c_vanila_corn, .blue_rect_for_i-c_vanila_cornchik, .blue_rect_for_i-c_wh-chokolate_stick, .plum_rect_for_i-c_mix, .plum_rect_for_banki"
+    ).css("display", "none"); //hiding MiniMenus
+  });
+});
+/* close .singlemenu_banki(#FFDBC7, SingleMenu) due to click on (img) .singlemenu_cross */
+$(function () {
+  $(".singlemenu_cross").on("click", function () {
+    $(".singlemenu_banki").fadeOut(100);
+  });
+});
+
+// Mini_Menu and SingleMenu end //
+
+// Big_massages starts //
+/* open .Love_big_massage_1(#EC5569, Big_massage) due to click on (img) .lovemassage_mini */
+$(function () {
+  $(".lovemassage_mini").on("click", function () {
+    $(".Love_big_massage_1").fadeToggle(650);
+  });
+});
+
+/* open .Love_big_massage_2_1(#EC5569, Big_massage) due to click on (img) .img-heart_love_big_massage_1 */
+$(function () {
+  $(".img-heart_love_big_massage_1").on("click", function () {
+    $(".Love_big_massage_2_1").fadeToggle(250).delay(4200).fadeOut(300); //through 4s hide .Love_big_massage_2_1
+    $(".Love_big_massage_1").fadeOut(370); // hide .Love_big_massage_1
+    $(".Love_big_massage_3")
+      .delay(4300)
+      .fadeToggle(250) // through 4s open .Love_big_massage_3
+      .delay(5000)
+      .fadeOut(250); // after 5s hide .Love_big_massage_3
+  });
+});
+
+/* open .Love_big_massage_2_2(#EC5569, Big_massage) due to click on (img) .img-brokenheart_love_big_massage_1
+  and hide .Love_big_massage_1 */
+$(function () {
+  $(".img-brokenheart_love_big_massage_1").on("click", function () {
+    $(".Love_big_massage_2_2").fadeToggle(250).delay(4200).fadeOut(300); // through 4s hide .Love_big_massage_2_2
+    $(".Love_big_massage_1").fadeOut(370); // hide .Love_big_massage_1
+    $(".Love_big_massage_3")
+      .delay(4300)
+      .fadeToggle(250) // through 4s open .Love_big_massage_3
+      .delay(5000)
+      .fadeOut(250); // after 5s hide .Love_big_massage_3
+  });
+});
+
+// Big_massages end //
